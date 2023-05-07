1a159e3347643077d83b5f770d3120c0bb3363bc
AnorakDWise <gregory442005@gmail.com>
2023-05-08 00:45:45 +0200

    made a cool animation tho

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css';
 import {sans} from "./fonts";
 
-
-
 export const metadata = {
   title: '2023CTF_hackathon',
   description: '2023 CTF hackathon waiting list',
@@ -15,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={sans.className + " m-auto"}>{children}</body>
+      <body className={sans.className + " m-auto bg-zinc-900 overflow-x-hidden"}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import {mono} from "./fonts";
+
 export default function Home() {
-  return (
-    <main className="">
-    </main>
-  )
+    return (
+        <main className="select-none">
+            <section className={"m-auto w-5/6 h-screen flex flex-col justify-center items-baseline"}>
+                <h1 className={mono.className + " text-green-400 text-9xl"}>#2023CTF_hackathon</h1>
+                <div className={"absolute bg-zinc-900 h-[150px] w-[2000px] consoleUnderline"}><div className={"animate-pulse"}><div className={"w-[75px] h-[20px] bg-zinc-100 translate-y-[80px]"}></div></div></div>
+                <h2 className={"text-5xl text-zinc-100 relative"}>Hack your way to the flag.</h2>
+            </section>
+        </main>
+    )
 }
