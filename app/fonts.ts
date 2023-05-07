9dc8ec1c0b2fafc07b63a505388b9b03ad3ccc5c
AnorakDWise <gregory442005@gmail.com>
2023-05-07 21:09:56 +0200

    init

---
+++ app/fonts.ts
@@ -0,0 +1,12 @@
+import {Roboto_Flex, Roboto_Mono} from "next/font/google";
+export const sans = Roboto_Flex({
+    display: "swap",
+    weight: ["300", "400", "500", "700"],
+    subsets: ["latin"]
+})
+
+export const mono = Roboto_Mono({
+    display: "swap",
+    weight: ["300", "400", "500", "700"],
+    subsets: ["latin"]
+})
